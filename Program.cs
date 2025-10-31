c4b3e5bdbbe81a9ba60ec205e2a62489750e21c4
nehad <Dark@DESKTOP-TQDVAO4>
2025-10-31 03:27:45 -0700

    1

--- Program.cs
+++ Program.cs
@@ -6,30 +6,32 @@
     {
         static void Main(string[] args)
         {
-            {
-                List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+            #region 1
+            //    {
+            //        List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
 
-                Console.WriteLine("Before reversing: " + string.Join(", ", numbers));
-                ReverseList(numbers);
-                Console.WriteLine("After reversing: " + string.Join(", ", numbers));
-            }
+            //        Console.WriteLine("Before reversing: " + string.Join(", ", numbers));
+            //        ReverseList(numbers);
+            //        Console.WriteLine("After reversing: " + string.Join(", ", numbers));
+            //    }
+            //}
+
+            //static void ReverseList<T>(List<T> list)
+            //{
+            //    int left = 0;
+            //    int right = list.Count - 1;
+
+            //    while (left < right)
+            //    {
+            //        T temp = list[left];
+            //        list[left] = list[right];
+            //        list[right] = temp;
+
+            //        left++;
+            //        right--;
+            //    }
+            //} 
+            #endregion
+
         }
-        static void ReverseList<T>(List<T> list)
-        {
-            int left = 0;
-            int right = list.Count - 1;
-
-            while (left < right)
-            {
-                // Swap elements
-                T temp = list[left];
-                list[left] = list[right];
-                list[right] = temp;
-
-                left++;
-                right--;
-            }
-        }
-    
-}
 }
