9a142c1d892e7df79e23b8c55d8b9a8609a20391
nehad <Dark@DESKTOP-TQDVAO4>
2025-10-31 03:29:30 -0700

    2

--- Program.cs
+++ Program.cs
@@ -4,8 +4,32 @@
 
     internal class Program
     {
+        #region 2
+        //static List<int> GetEvenNumbers(List<int> numbers)
+        //{
+        //    List<int> evenNumbers = new List<int>();
+
+        //    foreach (int num in numbers)
+        //    {
+        //        if (num % 2 == 0)
+        //        {
+        //            evenNumbers.Add(num);
+        //        }
+        //    }
+
+        //    return evenNumbers;
+        //} 
+        #endregion
         static void Main(string[] args)
         {
+            #region 2
+            //List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //List<int> evens = GetEvenNumbers(numbers);
+
+            //Console.WriteLine("Original list: " + string.Join(", ", numbers));
+            //Console.WriteLine("Even numbers: " + string.Join(", ", evens)); 
+            #endregion
             #region 1
             //    {
             //        List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
@@ -34,4 +58,5 @@
             #endregion
 
         }
-}
+
+    }
